59ff1cafbffbe5e4780ba081372de1a646cb3c69
Ahn <potensj@gmail.com>
2018-08-29 14:52:35 +0900

    Update index.js

--- router/join/index.js
+++ router/join/index.js
@@ -10,8 +10,8 @@ var LocalStrategy = require('passport-local').Strategy
 var connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
-  user: 'root',
-  password: '1234',
+  user: '****',
+  password: '****',
   database: 'jsman'
 })
 connection.connect()
